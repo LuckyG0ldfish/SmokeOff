de4b5a55d56c9f8f25b2f0ce896f5dd872d1bac8
LuckyG0ldfish <tim-koeneke@online.de>
2022-11-08 20:09:56 +0100

    trying stuff

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
+
     appBar: AppBar(
       title: Text("SmokeOff"),
       centerTitle: true,
     ),
-    body:
+    body: Center(
+      child:  FloatingActionButton(
+        onPressed: () {},
+        child: Text('idiots'),
+      )//Text('smart'),
+      // TextButton(
+        //onPressed: () {},
+      //),
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+      items: [BottomNavigationBarItem(
+        icon: Icon(),
+      ),],
+      fixedColor: Colors.green,
+    ),
   ),
 ));
